1ba173a0850e025faec95b8b8eebdad12e358f88
Shahrukh Anwar <shahrukh.anwar@mail.vinove.com>
2021-06-21 12:07:49 +0530

    ADDED | Transition code added

--- src/components/CompanyName.vue
+++ src/components/CompanyName.vue
@@ -7,7 +7,17 @@
 
 			<button @click="clickMe">Button from Child Component</button>
 		</div>
-	</div>
+
+		<div>
+			<button @click="handleApperance">Click me Toggle</button>
+			<transition name="slide-fade">
+				<span v-if="showSpan">
+					<p>This text is represinting TransitionThis text is represinting TransitionThis text is represinting TransitionThis text is represinting TransitionThis text is represinting Transition</p>
+					<span>This text is represinting Transition</span>
+				</span>	
+			</transition>
+		</div>
+	</div>	
 </template>
 
 <script>
@@ -20,10 +30,14 @@
 		},
 		data () {
 			return {
+				showSpan: false,
 				name: `This is new Component's Name State`
 			}
 		},
 		methods: {
+			handleApperance () {
+				this.showSpan = !this.showSpan
+			},
 			clickMe () {
 				this.parentName = 'New Value'
 				this.$emit('eventEmitted', {
@@ -34,8 +48,18 @@
 	}
 </script>
 
-<style>
+<style scoped>
 	.heading {
 		color: red;
 	}
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
